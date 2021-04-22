ab8afa54d1ee6ca6db4d509c55166098369b2b11
dwedwg <63416953+dwedwg@users.noreply.github.com>
2021-04-22 12:39:07 -0700

    Add files via upload
    starter files

---
+++ functions.cpp
@@ -0,0 +1,31 @@
+//Admin functions
+void getUser();
+void getBooks();
+
+//User functions
+String getName();
+setName();
+void isRegistered();
+bool editUser();
+String searchInventory();
+void addItemToCart();
+bool checkout();
+
+//Book functions
+void showBooks();
+Book getBooks();
+bool checkinBook();
+bool checkoutBook();
+
+int getWaitlistNum();
+
+
+//Order functions
+totalItems();
+printDetails();
+cancel();
+
+//Payment functions
+
+bool processPayment();
+int computeLateFee();
