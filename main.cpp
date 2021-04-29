4d826955c6a41f47dc1dbdda1ad510364fcca601
dwedwg <63416953+dwedwg@users.noreply.github.com>
2021-04-29 16:11:52 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,36 @@
 //main.cpp for Library
 #include 
+
+int main(void)
+{
+	int choice;
+	cout << "Library Assistant Main Menu" << endl;
+	cout << "Please pick an option below" << endl;
+	cin >> choice
+	do 
+	{
+		switch (choice)
+			case 1:
+				//View book Inventory
+				break;
+			case 2:
+				//
+				break;
+			case 3:
+				//
+				break;
+			case 4:
+				//Check In book
+				break;
+			case 5:
+				//Check out book
+				break;
+			case 6:
+				//Exit
+				break;
+	}while(choice >= 0 || choice <= 6)
+
+
+
+	return 0
+}
